62f992839517a5862bdefeb2dea8fa61610e1d21
ikarjala <98672330+ickarjala@users.noreply.github.com>
2022-05-14 14:36:10 +0300

    merge, change tilemap and objects scale, fix compile errors, picking up works

--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -3,12 +3,18 @@ using UnityEngine;
 public class PlayerInteract : MonoBehaviour
 {
 	public float radius;
+	public LayerMask itemMask;
 
 	public SpriteRenderer itemGfx;
 	public Rigidbody2D itemPrefab;
 
 	private RobotPart equippedPart;
 
+	private void Awake()
+	{
+		itemGfx.gameObject.SetActive(false);
+	}
+	
 	private void Update()
 	{
 		if (Input.GetKeyDown(KeyCode.E))
@@ -21,11 +27,12 @@ public class PlayerInteract : MonoBehaviour
 
 		private void PickUp()
 		{
-			Collider2D col = Physics2D.OverlapCircle(transform.position, radius, LayerMask.Interactables);
+			Collider2D col = Physics2D.OverlapCircle(transform.position, radius, itemMask);
 			if (col)
 				equippedPart = col.GetComponent<RobotPart>();
-			if (!equippedPart)
+			if (equippedPart == null)
 				return ;
+			Debug.Log("pickup object");
 			Sprite img = col.GetComponent<SpriteRenderer>().sprite;
 			Destroy(equippedPart.gameObject);
 			itemGfx.gameObject.SetActive(true);
@@ -35,8 +42,15 @@ public class PlayerInteract : MonoBehaviour
 		private void Drop()
 		{
 			itemGfx.gameObject.SetActive(false);
-			Rigidbody2D rb = Instantiate<RigidBody2D>(itemPrefab, transform.position, Quaternion.identity);
+			Rigidbody2D rb = Instantiate<Rigidbody2D>(itemPrefab, transform.position, Quaternion.identity);
 			rb.velocity = Vector2.zero;
 			equippedPart = null;
+		}
+
+		// DEBUG
+		private void OnDrawGizmosSelected()
+		{
+			Gizmos.color = Color.white;
+			Gizmos.DrawWireSphere(transform.position, radius);
 		}
 }
--- Assets/Scripts/RobotPart.cs
+++ Assets/Scripts/RobotPart.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class RobotPart
+public class RobotPart : MonoBehaviour
 {
 	
 }
