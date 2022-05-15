cd8e24d42461c7acb865485c396eb77f3d4728ca
ikarjala <98672330+ickarjala@users.noreply.github.com>
2022-05-15 13:48:05 +0300

    update charcontroller and player

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -9,92 +9,83 @@ public class CharacterController2D : MonoBehaviour
 {
     // Move player in 2D space
     public float maxSpeed = 3.4f;
+	public AnimationCurve acceleration;
+	public float accelTime = 1f;
+	[Space]
+	public LayerMask groundMask;
     public float jumpHeight = 6.5f;
 
-    bool facingRight = true;
-    float moveDirection = 0;
-    bool isGrounded = false;
-    Rigidbody2D r2d;
+	Player player;
+	Rigidbody2D r2d;
     CapsuleCollider2D mainCollider;
     Transform t;
+	SpriteRenderer rend;
+
+	private  float colliderRadius;
+    private bool isGrounded = false;
 
     // Use this for initialization
-    void Start()
+    private void Awake()
     {
         t = transform;
         r2d = GetComponent<Rigidbody2D>();
         mainCollider = GetComponent<CapsuleCollider2D>();
-
-        r2d.freezeRotation = true;
-        r2d.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
-        facingRight = t.localScale.x > 0;
+		rend = GetComponent<SpriteRenderer>();
+		player = GetComponent<Player>();
+		
+		colliderRadius = mainCollider.size.x * 0.4f * Mathf.Abs(transform.localScale.x);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Movement controls
-
-        if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) && (isGrounded || Mathf.Abs(r2d.velocity.x) > 0.01f))
-        {
-            moveDirection = Input.GetKey(KeyCode.A) ? -1 : 1;
-        }
-        else
-        {
-            if (isGrounded || r2d.velocity.magnitude < 0.01f)
-            {
-                moveDirection = 0;
-            }
-        }
-
-        // Change facing direction
-        if (moveDirection != 0)
-        {
-            if (moveDirection > 0 && !facingRight)
-            {
-                facingRight = true;
-                t.localScale = new Vector3(Mathf.Abs(t.localScale.x), t.localScale.y, transform.localScale.z);
-            }
-            if (moveDirection < 0 && facingRight)
-            {
-                facingRight = false;
-                t.localScale = new Vector3(-Mathf.Abs(t.localScale.x), t.localScale.y, t.localScale.z);
-            }
-        }
-
-        // Jumping
-        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
-        {
-            r2d.velocity = new Vector2(r2d.velocity.x, jumpHeight);
-        }
     }
 
     void FixedUpdate()
     {
-        Bounds colliderBounds = mainCollider.bounds;
-        float colliderRadius = mainCollider.size.x * 0.4f * Mathf.Abs(transform.localScale.x);
-        Vector3 groundCheckPos = colliderBounds.min + new Vector3(colliderBounds.size.x * 0.5f, colliderRadius * 0.9f, 0);
-        // Check if player is grounded
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheckPos, colliderRadius);
-        //Check if any of the overlapping colliders are not player collider, if so, set isGrounded to true
-        isGrounded = false;
-        if (colliders.Length > 0)
-        {
-            for (int i = 0; i < colliders.Length; i++)
-            {
-                if (colliders[i] != mainCollider)
-                {
-                    isGrounded = true;
-                    break;
-                }
-            }
-        }
+		Vector2 velocity = r2d.velocity;
+		isGrounded = CheckGround();
+		
+        // Calculate movement velocity
+		velocity.x = player.moveDir.x * maxSpeed;
 
-        // Apply movement velocity
-        r2d.velocity = new Vector2((moveDirection) * maxSpeed, r2d.velocity.y);
+		// Jumping
+		if (player.queJump && isGrounded)
+			velocity.y = Mathf.Sqrt(-2f * jumpHeight * Physics2D.gravity.y * r2d.gravityScale);
 
-        // Simple debug
-        Debug.DrawLine(groundCheckPos, groundCheckPos - new Vector3(0, colliderRadius, 0), isGrounded ? Color.green : Color.red);
-        Debug.DrawLine(groundCheckPos, groundCheckPos - new Vector3(colliderRadius, 0, 0), isGrounded ? Color.green : Color.red);
+		// Apply forces
+		r2d.velocity = velocity;
+
+		// Update Graphic
+		if (!rend.flipX && r2d.velocity.x < 0f)
+			rend.flipX = true;
+		else if (rend.flipX && r2d.velocity.x > 0f)
+			rend.flipX = false;
     }
+
+	private bool CheckGround()
+	{
+		Bounds colliderBounds = mainCollider.bounds;
+		Vector3 groundCheckPos = colliderBounds.min + new Vector3(colliderBounds.size.x * 0.5f, colliderRadius * 0.9f, 0);
+
+		// Simple debug
+		//        Debug.DrawLine(groundCheckPos, groundCheckPos - new Vector3(0, colliderRadius, 0), isGrounded ? Color.green : Color.red);
+		//        Debug.DrawLine(groundCheckPos, groundCheckPos - new Vector3(colliderRadius, 0, 0), isGrounded ? Color.green : Color.red);
+		
+		// Check if player is grounded
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheckPos, colliderRadius, groundMask);
+		foreach (Collider2D c in colliders)
+			if (c != mainCollider)
+				return (true);
+		return (false);
+	}
+
+private void OnDrawGizmosSelected()
+{
+	if (mainCollider == null)
+		mainCollider = GetComponent<CapsuleCollider2D>();
+	Gizmos.color = Color.yellow;
+	Gizmos.DrawWireCube(new Vector3(mainCollider.bounds.center.x, mainCollider.bounds.min.y + jumpHeight * .5f, 0f), new Vector3(.4f, jumpHeight, 0f));
+}
+
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,11 +3,24 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 	[HideInInspector]
-	public Vector2 rawMoveDir;
+	public Vector2 moveDir;
+	public bool queJump = false;
 
+	private const float jumpBufferTime = .2f;
+	private float jumpBuffer = 0f;
+	
 	private void Update()
 	{
-		rawMoveDir.x = Input.GetAxisRaw("Horizontal");
-		rawMoveDir.y = Input.GetAxisRaw("Vertical");
+		moveDir.x = Input.GetAxis("Horizontal");
+		moveDir.y = Input.GetAxis("Vertical");
+
+		jumpBuffer -= Time.deltaTime;
+		if (Input.GetButtonDown("Jump"))
+		{
+			queJump = true;
+			jumpBuffer = jumpBufferTime;
+		}
+		else if (jumpBuffer <= 0f)
+			queJump = false;
 	}
 }
