c99529493635c7f747933e886346c9d9779f3f02
Daniel Palacio <dpalacio@student.hive.fi>
2022-05-15 17:07:33 +0300

    starting to work on build order score increase

--- Assets/Scripts/RobotPart.cs
+++ Assets/Scripts/RobotPart.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class RobotPart : MonoBehaviour
@@ -5,6 +7,9 @@ public class RobotPart : MonoBehaviour
 	private GameManager gameManager;
 	private float rightDestroy = 18;
 	private float leftDestroy = -27;
+	public static int topCount = 0;
+	public static int midCount = 0;
+	public static int bottomCount = 0;
 	public enum RobotPartType
 	
 	{
@@ -33,13 +38,89 @@ public class RobotPart : MonoBehaviour
     {
         if (transform.position.x < leftDestroy)
         {
-			gameManager.UpdateScore(1);
+			//gameManager.UpdateScore(1);
             Destroy(gameObject);
         }
 		if (transform.position.x > rightDestroy)
         {
-			gameManager.UpdateScore(1);
+			//gameManager.UpdateScore(1);
             Destroy(gameObject);
         }
     }
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("GoalTop"))
+		{
+			if (topCount == 0 && CompareTag("RobotHead"))
+			{
+				topCount++;
+			}
+			else if (topCount == 1 && CompareTag("RobotTorso"))
+			{
+				topCount++;
+			}
+			else if (topCount == 2 && CompareTag("RobotLegs"))
+			{
+				topCount++;
+			}
+			else if (topCount == 3)
+			{
+				gameManager.UpdateScore(1);
+				topCount = 0;
+			}
+			else 
+			{
+				topCount = 0;
+			}
+			
+		}
+		if (other.gameObject.CompareTag("GoalMid"))
+		{
+			if (midCount == 0 && CompareTag("RobotHead"))
+			{
+				topCount++;
+			}
+			else if (midCount == 1 && CompareTag("RobotTorso"))
+			{
+				topCount++;
+			}
+			else if (midCount == 2 && CompareTag("RobotLegs"))
+			{
+				midCount++;
+			}
+			else if (midCount == 3)
+			{
+				gameManager.UpdateScore(1);
+				midCount = 0;
+			}
+			else 
+			{
+				midCount = 0;
+			}
+		}
+		if (other.gameObject.CompareTag("GoalBottom"))
+		{
+			if (bottomCount == 0 && CompareTag("RobotLegs"))
+			{
+				bottomCount++;
+			}
+			else if (bottomCount == 1 && CompareTag("RobotTorso"))
+			{
+				topCount++;
+			}
+			else if (bottomCount == 2 && CompareTag("RobotHead"))
+			{
+				bottomCount++;
+			}
+			else if (topCount == 3)
+			{
+				gameManager.UpdateScore(1);
+				bottomCount = 0;
+			}
+			else 
+			{
+				bottomCount = 0;
+			}
+		}
+	}
 }
